25f573746ac942b518cad4abc713438319763bee
Isak Kvalvaag Torgersen <isak.torgersen@gmail.com>
2019-02-21 15:18:26 +0100

    Finished tasks 1-3. Changed layout to fixed left  menu and fixed header. Selected menue item is now highlighted

---
+++ Assignment1/Assignment1.Web/wwwroot/js/navigation.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var url = window.location.href;
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+    url = url.substr(url.lastIndexOf("/") + 1);
+
+    if (url == "") {
+        url = "Home";
+    }
+
+    $(".sidenav a").removeClass("activeMenuItem");
+    $(".sidenav a[url='" + url + "']").addClass("activeMenuItem");
+
+    console.log(url);
+
+});
